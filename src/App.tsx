8f10675107e5f72a92c8ad76dd288c8dce36e05c
Nino Filiu <nino.filiu@gmail.com>
2024-10-11 22:13:30 +0200

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export const App = () => (
+  <div className="main">
+    <div>
+      to: <input type="text" disabled value="ethelreal666" />
+    </div>
+    <textarea disabled rows={7}>
+      hey Ethel! i know it's kinda random to msg u after not talking to u for
+      years but guess what happened today!! well remember the abandonned house
+      near my place?? it's gettin renovated, i saw construction workers painting
+      the whole thing "gentrification turquoise" xD idk it made me think abt u
+      cuz we used to hangout there all the time.. anyway what's up how r u???
+    </textarea>
+  </div>
+);
