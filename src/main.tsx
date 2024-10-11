87877ecbe3f1e7670cfddd551fbaa5844ed2ff76
Nino Filiu <nino.filiu@gmail.com>
2024-10-11 22:18:28 +0200

    react-router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<StrictMode />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              home
+              <Link to="/1-123456789">weird</Link>
+            </>
+          }
+        />
+        <Route
+          path="/1-123456789"
+          element={
+            <>
+              weird
+              <Link to="/">home</Link>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
