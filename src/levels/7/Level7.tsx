46124683bb0a55aa33ab2d419b87860241cefb86
Nino Filiu <nino.filiu@gmail.com>
2024-10-12 23:22:25 +0200

    lvl7: images

--- src/levels/7/Level7.tsx
+++ src/levels/7/Level7.tsx
@@ -1,38 +1,60 @@
+import styled from "@emotion/styled";
 import { useState } from "react";
+
+const Puzzle = styled.div`
+  display: grid;
+  margin: auto;
+  width: calc(100vmin);
+  height: calc(100vmin);
+  grid-template-columns: 1fr 1fr 1fr;
+  button {
+    width: calc(100vmin / 3);
+    height: calc(100vmin / 3);
+    background: transparent;
+    border: none;
+    padding: 0;
+    margin: 0;
+  }
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export const Level7 = () => {
   const [map, setMap] = useState([
-    ["00", "10", "20"],
-    ["01", "11", "21"],
-    ["xx", "12", "22"],
+    ["ethel20.png", "ethel12.png", "ethel21.png"],
+    ["ethel11.png", null, "ethel10.png"],
+    ["ethel01.png", "ethel22.png", "ethel00.png"],
   ]);
   const [s, setS] = useState(null as null | { x: number; y: number });
+  console.log(map);
   return (
-    <div>
-      {[0, 1, 2].map((y) => (
-        <div key={y}>
-          {[0, 1, 2].map((x) => (
-            <button
-              key={x}
-              onClick={() => {
-                if (map[y][x] == "xx") {
-                  if (s === null) return;
-                  [map[y][x], map[s.y][s.x]] = [map[s.y][s.x], map[y][x]];
-                  setMap(structuredClone(map));
-                  setS(null);
-                } else {
-                  setS({ x, y });
-                }
-              }}
-              style={{
-                color: s && s.x === x && s.y === y ? "red" : "white",
-              }}
-            >
-              {map[y][x]}
-            </button>
-          ))}
-        </div>
-      ))}
-    </div>
+    <Puzzle>
+      {[0, 1, 2].flatMap((y) =>
+        [0, 1, 2].map((x) => (
+          <button
+            key={3 * y + x}
+            onClick={() => {
+              if (map[y][x] === null) {
+                if (s === null) return;
+                if (Math.abs(x - s.x) + Math.abs(y - s.y) > 1) return;
+                [map[y][x], map[s.y][s.x]] = [map[s.y][s.x], map[y][x]];
+                setMap(structuredClone(map));
+                setS(null);
+              } else {
+                setS({ x, y });
+              }
+            }}
+            style={{
+              opacity: s && s.x === x && s.y === y ? 0.2 : 1,
+            }}
+          >
+            {map[y][x] && <img src={map[y][x]} />}
+          </button>
+        ))
+      )}
+    </Puzzle>
   );
 };
