a97d2c58e7a32d2ce84bee58ce8f8212b03d8a69
Nino Filiu <nino.filiu@gmail.com>
2024-10-12 21:46:05 +0200

    lvl7: slide core

--- src/levels/7/Level7.tsx
+++ src/levels/7/Level7.tsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+
 export const Level7 = () => {
-  return <></>;
+  const [map, setMap] = useState([
+    ["00", "10", "20"],
+    ["01", "11", "21"],
+    ["xx", "12", "22"],
+  ]);
+  const [s, setS] = useState(null as null | { x: number; y: number });
+  return (
+    <div>
+      {[0, 1, 2].map((y) => (
+        <div key={y}>
+          {[0, 1, 2].map((x) => (
+            <button
+              key={x}
+              onClick={() => {
+                if (map[y][x] == "xx") {
+                  if (s === null) return;
+                  [map[y][x], map[s.y][s.x]] = [map[s.y][s.x], map[y][x]];
+                  setMap(structuredClone(map));
+                  setS(null);
+                } else {
+                  setS({ x, y });
+                }
+              }}
+              style={{
+                color: s && s.x === x && s.y === y ? "red" : "white",
+              }}
+            >
+              {map[y][x]}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
